3d444a978f541509fc860ce27790496eca84cbfd
Edu7228 <msgboxedu@gmail.com>
2026-08-09 15:03:57 -0300

    ConexaoBanco.php

--- Backend/PHP/ConexaoBanco.php
+++ Backend/PHP/ConexaoBanco.php
@@ -1,1 +1,27 @@
 
+
+<?php
+
+class Conexao
+{
+    private $host = "localhost";
+    private $usuario = "root";
+    private $senha = "";
+    private $banco = "videogame";
+
+    public function Conectar()
+    {
+        $conn = new mysqli(
+            $this->host,
+            $this->usuario,
+            $this->senha,
+            $this->banco
+        );
+
+        if ($conn->connect_error) {
+            die("Erro de conexão: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
